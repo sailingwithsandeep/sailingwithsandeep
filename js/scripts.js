7e7bfd1baee3b03642d2a423bd47d6499d102e2c
SandeepParmar-RKU <sparmar586@rku.ac.in>
2021-04-19 20:21:23 +0530

    Final cut\

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+/**
+*
+* -------------------------------------------------
+*
+* Template : Myfolio - Personal Portfolio Template
+* Author : thecodude
+* Author URI : http://thecodrops.com
+*
+* --------------------------------------------------
+*
+**/
+
+(function ($) {
+    'use strict';
+    jQuery(document).ready(function () {
+		// Counter
+		$('.counter').countTo();
+    });
+})(jQuery);
